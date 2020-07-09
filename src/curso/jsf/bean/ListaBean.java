03d717c7aab3dcb59b4710c8d9f0c2fd9fe8db98
januario86 <januario86fp@gmail.com>
2020-07-08 22:27:53 -0300

    Criação da classe ListaBean

---
+++ src/curso/jsf/bean/ListaBean.java
@@ -0,0 +1,47 @@
+package curso.jsf.bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name= "listaBean")
+public class ListaBean implements Serializable{
+
+	private String item;
+	
+	private List<String> itens = new ArrayList<String>();
+	
+	public String adicionar() {
+		itens.add(item);
+		item = null;
+		return null;
+		
+	}
+	
+	public String removerTudo() {
+		itens.clear();
+		
+		return null;
+		
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public List<String> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<String> itens) {
+		this.itens = itens;
+	}
+		
+	
+}
